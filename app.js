0e7278c4ae5950012f7156e15e15c0b89dcfd995
André Brinkop <andre@brinkop.de>
2020-04-17 00:24:23 +0200

    improve error handling

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const multer = require("multer");
 const sharp = require('sharp');
 
 var app = express();
+var imageUpload = multer().single('image');
 
-var storage = multer.memoryStorage();
-var upload = multer({ storage: multer.memoryStorage() });
-
-app.post('/getInfo', upload.single('image'), function (req, res) {
-    const image = sharp(req.file.buffer);
-    image
-        .metadata()
-        .then(function (metadata) {
-            res.send({name: req.file.originalname, format: metadata.format, height: metadata.height, width: metadata.width});
-        }).catch(function (error) {
-            res.status(422).send("Invalid 'image' file.");
+app.post('/getInfo', function (req, res) {
+    imageUpload(req, res, function (err) {
+        if (err || req.file == undefined) {
+            return res.status(400).end("Invalid request. Make sure the request body contains an image.");
+        }
+        const image = sharp(req.file.buffer);
+        image.metadata().then(function (metadata) {
+                return res.send({name: req.file.originalname, format: metadata.format, height: metadata.height, width: metadata.width});
+            }).catch(function (error) {
+            return res.status(422).send("Invalid 'image' file.");
         });
+    });
 });
 
 
