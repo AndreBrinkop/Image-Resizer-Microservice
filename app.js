8fd9d3b1a7aa6c8f0d7493bdc22554f2a5c19165
André Brinkop <andre@brinkop.de>
2020-04-16 20:53:38 +0200

    implement getInfo post endpoint that receives an image and returns the images name, format, height and width

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const multer = require("multer");
 const sharp = require('sharp');
+
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: multer.memoryStorage() });
+
+app.post('/getInfo', upload.single('image'), function (req, res) {
+    const image = sharp(req.file.buffer);
+    image
+        .metadata()
+        .then(function (metadata) {
+            res.send({name: req.file.originalname, format: metadata.format, height: metadata.height, width: metadata.width});
+        }).catch(function (error) {
+            res.status(422).send("Invalid 'image' file.");
+        });
 });
+
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
