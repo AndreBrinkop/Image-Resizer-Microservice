87028f94978ab504d2221c0d55b3ffbd258d1b16
André Brinkop <andre@brinkop.de>
2020-04-17 01:33:58 +0200

    add resize-image post endpoint that takes an images and dimensions and returns the resized image

--- app.js
+++ app.js
@@ -12,10 +12,41 @@ app.post('/image-details', function (req, res) {
         }
         const image = sharp(req.file.buffer);
         image.metadata().then(function (metadata) {
-                return res.send({name: req.file.originalname, format: metadata.format, height: metadata.height, width: metadata.width});
-            }).catch(function (error) {
+            return res.send({
+                name: req.file.originalname,
+                format: metadata.format,
+                width: metadata.width,
+                height: metadata.height
+            });
+        }).catch(function (error) {
             return res.status(422).send("Invalid 'image' file.");
         });
+    });
+});
+
+app.post('/resize-image', function (req, res) {
+    imageUpload(req, res, function (err) {
+        if (err || req.file == undefined) {
+            return res.status(400).end("Invalid request. Make sure the request body contains an image.");
+        }
+        const height = parseInt(req.body.height);
+        const width = parseInt(req.body.width);
+        if (isNaN(height) || height < 1) {
+            return res.status(422).end("Invalid request. Make sure the 'height' parameter is a positive integer value.");
+        }
+        if (isNaN(width) || width < 1) {
+            return res.status(422).end("Invalid request. Make sure the 'width' parameter is a positive integer value.");
+        }
+
+        const image = sharp(req.file.buffer);
+        image.resize(width, height, { fit: 'contain' })
+            .toBuffer()
+            .then(function (resized) {
+                return res.end(resized, 'binary');
+            })
+            .catch(function (error) {
+                return res.status(422).send("Invalid 'image' file.");
+            });
     });
 });
 
