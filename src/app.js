f4ec17e1491aeac4f9d377b670ffbfd9d91603de
André Brinkop <andre@brinkop.de>
2020-04-17 14:43:03 +0200

    es6 refactoring

--- src/app.js
+++ src/app.js
@@ -2,30 +2,29 @@ const express = require('express')
 const multer = require('multer')
 const sharp = require('sharp')
 
-var app = express()
-var imageUpload = multer().single('image')
+const app = express()
+const imageUpload = multer().single('image')
 
-app.post('/image-details', function (req, res) {
-  imageUpload(req, res, function (err) {
+app.post('/image-details', (req, res) => {
+  imageUpload(req, res, err => {
     if (err || req.file === undefined) {
       return res.status(400).end('Invalid request. Make sure the request body contains an image.')
     }
     const image = sharp(req.file.buffer)
-    image.metadata().then(function (metadata) {
+    image.metadata().then(metadata => {
       return res.send({
         name: req.file.originalname,
         format: metadata.format,
         width: metadata.width,
         height: metadata.height
       })
-    }).catch(function () {
-      return res.status(422).send("Invalid 'image' file.")
-    })
+    }).catch(() =>
+      res.status(422).send("Invalid 'image' file."))
   })
 })
 
-app.post('/resize-image', function (req, res) {
-  imageUpload(req, res, function (err) {
+app.post('/resize-image', (req, res) => {
+  imageUpload(req, res, err => {
     if (err || req.file === undefined) {
       return res.status(400).end('Invalid request. Make sure the request body contains an image.')
     }
@@ -41,17 +40,15 @@ app.post('/resize-image', function (req, res) {
     const image = sharp(req.file.buffer)
     image.resize(width, height, { fit: 'contain' })
       .toBuffer()
-      .then(function (resized) {
+      .then(resized => {
         return res.end(resized, 'binary')
       })
-      .catch(function () {
+      .catch(() => {
         return res.status(422).send("Invalid 'image' file.")
       })
   })
 })
 
-const server = app.listen(3000, function () {
-  console.log('ImageResizer is listening on port 3000!')
-})
+const server = app.listen(3000, () => console.log('ImageResizer is listening on port 3000!'))
 
 exports = module.exports = server
