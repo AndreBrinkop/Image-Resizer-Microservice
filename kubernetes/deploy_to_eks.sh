3e5776979b79a69d6ac037d01738fe16b1eccef9
André Brinkop <andre@brinkop.de>
2020-04-19 20:45:06 +0200

    fix newline in log

--- kubernetes/deploy_to_eks.sh
+++ kubernetes/deploy_to_eks.sh
@@ -2,15 +2,15 @@
 CLUSTER_NAME='ImageResizerMicroservice'
 REGION_CODE='us-east-1'
 
-echo "Setting up kubectl config"
+echo 'Setting up kubectl config'
 aws eks --region $REGION_CODE update-kubeconfig --name $CLUSTER_NAME
 
-echo "Deploy application and setup Load Balancer"
+echo 'Deploy application and setup Load Balancer'
 kubectl apply -f deployment.yml
 
 sleep 5
 
-echo "\n"
-echo "Image Resizer Microservice (Load Balancer) Hostname:"
+echo '\n'
+echo 'Image Resizer Microservice (Load Balancer) Hostname:'
 hostname=$(kubectl get services/image-resizer-service -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 echo $hostname
