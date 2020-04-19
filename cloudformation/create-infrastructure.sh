46097183c1362eb0e784bcda3dddb6ad1fcc2041
André Brinkop <andre@brinkop.de>
2020-04-19 13:35:35 +0200

    add script to build infrastructure

---
+++ cloudformation/create-infrastructure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+REGION_CODE='us-west-1'
+
+NETWORK_STACK_NAME='ImageResizerNetwork'
+aws --region $REGION_CODE cloudformation create-stack --stack-name $NETWORK_STACK_NAME --template-body file://network.yml --parameters file://network-params.json
+aws --region $REGION_CODE cloudformation wait stack-create-complete --stack-name $NETWORK_STACK_NAME
+
+CLUSTER_STACK_NAME='ImageResizerCluster'
+aws --region $REGION_CODE cloudformation create-stack --stack-name $CLUSTER_STACK_NAME --template-body file://cluster.yml --parameters file://cluster-params.json --capabilities CAPABILITY_NAMED_IAM
+aws --region $REGION_CODE cloudformation wait stack-create-complete --stack-name $CLUSTER_STACK_NAME
